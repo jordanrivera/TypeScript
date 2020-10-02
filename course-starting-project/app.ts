960befe4103593c9617bc6c0c0ebac5b85736f85
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-03 00:10:55 +0200

    commit

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,5 +1,14 @@
-function add(n1: number, n2: number){
-    const result = n1 + n2;
+function combine(input1: number | string, input2: number | string){
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number'){
+        const result = input1 + input2
+    } else{
+        result = input1.toString() + input2.toString();
+    }
      return result;
     }
- 
+    
+    const combinedAges = combine(30, 26);
+    console.log(combinedAges);
+
+    const combinedNames = combine('Max', 'Anna');
