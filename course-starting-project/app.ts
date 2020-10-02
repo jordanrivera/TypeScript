9a39d1af72609850609a985e9a8a7c88e75481c4
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-02 23:44:04 +0200

    commit

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,14 +1,18 @@
-//const person: {
-//    name: string; 
-//    age: number;
-//
-//    nickname: string; 
+
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
 const person = { 
     name: 'Maximilian',
     age: 30,
     nickname: 'Loco',
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: ADMIN
 };
+
+//person.role.push('admin');
+//person.role[1] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
@@ -16,4 +20,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies){
     console.log(hobby.toUpperCase)
+}
+
+if (person.role === ADMIN){
+    console.log('is admin')
 }
