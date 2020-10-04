fbb0efc01ff1794effea6e055870b5327c6c104b
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-04 12:39:12 +0200

    commit

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -8,8 +8,9 @@ if (typeof userInput === 'string'){
     userName = userInput;
 }
 
-function generateError(message: string, code: number){
+function generateError(message: string, code: number): never{
     throw {message: message, errorCode: code};
+    //whiel (true) {}
     
 }
 const result = generateError('An error ocurred!', 500);
