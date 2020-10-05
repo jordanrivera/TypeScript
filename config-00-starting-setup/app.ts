ca488be7f0e861f4e75b79d8d913311aed139ede
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-05 09:32:37 +0200

    commit

--- config-00-starting-setup/app.ts
+++ config-00-starting-setup/app.ts
@@ -3,3 +3,8 @@ age = 30;
 const userName = 'Maximilian';
 
 console.log(userName);
+const button = document.querySelector('button');
+
+button?.addEventListener('click', () => {
+    console.log('Clicked!');
+});
