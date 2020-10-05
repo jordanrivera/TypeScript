18e235035972883bc2bffa0ef60371836fc23978
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-05 15:16:01 +0200

    commit

--- config-00-starting-setup/src/app.ts
+++ config-00-starting-setup/src/app.ts
@@ -1,3 +1,11 @@
+let appId = 'abc';
+function add(n1: number, n2: number){
+    if (n1 + n2 > 0){
+        return n1 + n2;
+    }
+    return;
+}
+
 let age: number;
 age = 30;
 const userName = 'Maximilian';
