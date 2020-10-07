3b6d1ef15599eadce8a60dd5817c4346987dfa65
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-07 13:03:58 +0200

    commit

---
+++ Classes/src/app.ts
@@ -0,0 +1,17 @@
+class Department {
+    name: string;
+
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    describe(){
+        console.log('Department: ' + this.name);
+    }
+}
+
+const accounting = new Department('Accounting');
+console.log(accounting);
+
+const accountingCopy = { describe: accounting.describe};
+accountingCopy.describe();
