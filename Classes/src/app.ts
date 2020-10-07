4ef26a1ec8d9a886b06d59fff695939852ae019e
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-07 22:43:56 +0200

    commit

--- Classes/src/app.ts
+++ Classes/src/app.ts
@@ -1,9 +1,9 @@
 class Department {
-    //private readonly id: string;
-    //private name: string;
-    private employees: string[] = [];
+    //public readonly id: string;
+    //public name: string;
+    protected employees: string[] = [];
 
-    constructor(private readonly id: string, public name: string) {
+    constructor(public readonly id: string, public name: string) {
         //this.id = id;
         //this.name = n;
     
@@ -24,13 +24,54 @@ class Department {
     }
 }
 
-const accounting = new Department('d1','Accounting');
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
-//accounting.employees[2] = 'Anna';
-accounting.describe();
-accounting.printEmployeeInformation();
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]){
+      super(id, 'IT');
+      this.admins = admins;
+    }
+}
 
+class AccountingDepartment extends Department{
+    constructor(id: string, private reports: string[]){
+        super(id, 'Accounting');
+    }
+    
+    addEmployee(name: string){
+        if (name === 'Max'){
+            return;
+        }
+        this.employees.push(name);
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+    }
+
+    getReports() {
+        console.log(this.reports);
+    }
+
+    printReports(){
+        console.log(this.reports);
+    }
+}
+
+
+const it = new ITDepartment('d1', ['Max']);
+const accounting2 = new Department('d1','Accounting');
+it.addEmployee('Max');
+it.addEmployee('Manu');
+//accounting.employees[2] = 'Anna';
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+
+console.log(it);
+const accounting = new AccountingDepartment('d2', []);
+accounting.addReport('Something went wrong...');
+accounting.addReport('Max');
+accounting.addEmployee('Manu');
+accounting.printReports();
 //console.log(accounting);
 //const accountingCopy = { name: 'DUMMY', describe: accounting.describe};
 //accountingCopy.describe();
