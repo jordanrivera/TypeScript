55b0965f103a75714847c5de3ecc383231134e46
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-07 15:07:35 +0200

    commit

--- Classes/src/app.ts
+++ Classes/src/app.ts
@@ -1,17 +1,36 @@
 class Department {
-    name: string;
+    //private readonly id: string;
+    //private name: string;
+    private employees: string[] = [];
 
-    constructor(n: string) {
-        this.name = n;
+    constructor(private readonly id: string, public name: string) {
+        //this.id = id;
+        //this.name = n;
+    
     }
 
-    describe(){
-        console.log('Department: ' + this.name);
+    describe(this: Department){
+        console.log('Department (${this.id}): ${this.name}');
+    }
+
+    addEmployee(employee:string){
+        //this.id - 'd2';
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
-const accounting = new Department('Accounting');
-console.log(accounting);
+const accounting = new Department('d1','Accounting');
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+//accounting.employees[2] = 'Anna';
+accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { describe: accounting.describe};
-accountingCopy.describe();
+//console.log(accounting);
+//const accountingCopy = { name: 'DUMMY', describe: accounting.describe};
+//accountingCopy.describe();
