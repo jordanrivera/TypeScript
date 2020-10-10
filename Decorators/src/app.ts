c8475a030c867050166fc013d0089b57c7406afd
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-11 00:11:44 +0200

    commit

--- Decorators/src/app.ts
+++ Decorators/src/app.ts
@@ -1,4 +1,5 @@
 function Logger(logString: string){
+    console.log('LOGGER FACTORY');
     return function(constructor: Function){
         console.log(logString);
         console.log(constructor);
@@ -6,7 +7,9 @@ function Logger(logString: string){
 };
 
 function WithTemplate(template: string, hookId: string){
+    console.log('TEMPLATE FACTORY');
     return function(constructor: any){
+        console.log('Rendering template');
         const hookEl = document.getElementById(hookId);
         const p = new constructor();
         if (hookEl) {
@@ -18,6 +21,7 @@ function WithTemplate(template: string, hookId: string){
 
 
 //@Logger('LOGGING - PERSON')
+@Logger('LOGGING')
 @WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = 'Max';
@@ -30,3 +34,47 @@ class Person {
 const pers = new Person();
 console.log(pers);
 
+//----
+
+function Log(target: any, propertyName: string | Symbol){
+    console.log('Property decorator!');
+    console.log(target, propertyName);
+}
+
+function Log2(target:any, name: string, descriptor: PropertyDescriptor ){
+    console.log('Accessor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor){
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+class Product {
+    @Log
+    title: string;
+    private _price: number;
+
+    @Log2
+    set price(val: number){
+        if (val > 0){
+            this._price = val;
+        }else{
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+
+    constructor(t: string, p: number){
+        this.title = t;
+        this._price = p;
+    }
+    @Log3
+    getPriceWithTax(tax: number){
+        return this._price * (1 + tax);
+    }
+}
