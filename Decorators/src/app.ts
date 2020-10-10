2a640e37f84cfa53750feb52053ff15473811eb6
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-11 00:37:27 +0200

    commit

--- Decorators/src/app.ts
+++ Decorators/src/app.ts
@@ -8,15 +8,19 @@ function Logger(logString: string){
 
 function WithTemplate(template: string, hookId: string){
     console.log('TEMPLATE FACTORY');
-    return function(constructor: any){
-        console.log('Rendering template');
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name;
+    return function<T extends {new (...args: any[]): {name: string}} >(originalConstructor: T){
+        return class extends originalConstructor {
+            constructor(..._: any[]){
+                super();
+                console.log('Rendering template');
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name;
+                }
+            }
         }
-    }
+    };
 }
 
 
@@ -46,6 +50,7 @@ function Log2(target:any, name: string, descriptor: PropertyDescriptor ){
     console.log(target);
     console.log(name);
     console.log(descriptor);
+    
 }
 
 function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor){
@@ -53,6 +58,13 @@ function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor
     console.log(target);
     console.log(name);
     console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number){
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
 }
 
 class Product {
@@ -74,7 +86,7 @@ class Product {
         this._price = p;
     }
     @Log3
-    getPriceWithTax(tax: number){
+    getPriceWithTax(@Log4 tax: number){
         return this._price * (1 + tax);
     }
 }
