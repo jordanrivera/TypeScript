33b9f786a391e8b812d2589177cd4e157546f71c
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-10 13:07:23 +0200

    commit

--- Decorators/src/app.ts
+++ Decorators/src/app.ts
@@ -1,3 +1,24 @@
+function Logger(logString: string){
+    return function(constructor: Function){
+        console.log(logString);
+        console.log(constructor);
+    }  
+};
+
+function WithTemplate(template: string, hookId: string){
+    return function(constructor: any){
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1')!.textContent = p.name;
+        }
+    }
+}
+
+
+//@Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = 'Max';
 
@@ -8,3 +29,4 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
