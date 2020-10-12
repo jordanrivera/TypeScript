38eabecf76dc2a432cd92225cae630b4359e3dda
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-12 17:24:07 +0200

    Update app.ts

--- prj-app-typescrpit/src/app.ts
+++ prj-app-typescrpit/src/app.ts
@@ -1,5 +1,5 @@
 //Validation 
-interface Validatbale {
+interface Validatable {
     value: string | number;
     required?: boolean;
     minLength?: number;
@@ -8,8 +8,26 @@ interface Validatbale {
     max: number;
 }
 
-function validate(validatableInput: ValidityState){
-    
+function validate(validatableInput: Validatable){
+    let isValid = true;
+    if (validatableInput.required){
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null && 
+        typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null && 
+        typeof validatableInput.value === 'string'){
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (validatableInput.min != null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number'){
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
 }
 
 //autobind decorator
@@ -54,10 +72,31 @@ class ProjectInput {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true,
+            min: 0,
+            max: 25
+
+        };
+        const discriptionValidatable: Validatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5,
+            min:0,
+            max:30
+        };
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        };
+
         if (
-            validate({value: enteredTitle,require: true, minLength: 5 }) &&
-            validate({value: enteredDescription, require: true, minLength: 5}) &&
-            validate({value: enteredPeople, require: true, minLength: 5})
+            validate(titleValidatable) &&
+            validate(discriptionValidatable) &&
+            validate(peopleValidatable)
         ){
             alert('Invalid input, please try again!');
             return;
