45209346b4f1618b83b62d1f1e6f0672bca4f140
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-12 13:46:13 +0200

    commit

--- prj-app-typescrpit/src/app.ts
+++ prj-app-typescrpit/src/app.ts
@@ -2,6 +2,10 @@ class ProjectInput {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement;
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -9,8 +13,22 @@ class ProjectInput {
    
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+        this.configure();
         this.attach();
     }
+    private submitHandler(event: Event){
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+
+    private configure(){
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
+
     private attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
