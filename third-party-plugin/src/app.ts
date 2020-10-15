4a54d15c148cb3ab51ad85ac59f3510d819aa930
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-15 16:44:21 +0200

    commit

--- third-party-plugin/src/app.ts
+++ third-party-plugin/src/app.ts
@@ -1,9 +1,4 @@
-import {ProjectInput} from './component/project-input';
-import {ProjectList} from  './component/project-list';
 
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
 
 
 
