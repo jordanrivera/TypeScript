cbbe36edea317148a730290f3aa01c2dddd24587
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-16 07:29:51 +0200

    commit

--- third-party-plugin/src/app.ts
+++ third-party-plugin/src/app.ts
@@ -1,4 +1,29 @@
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { Product } from './product.models';
+import {} from 'class-validator';
+import { validate } from "../node_modules/class-validator/types/index";
 
 
+const products = [
+    {title: 'A Carpet', price: 29.99}, 
+    {title: 'A Book', price: 10.99}];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then(errors => {
+    if (errors.length > 0){
+        console.log('Validation ERRORS!');
+        console.log(errors);
+    }else{
+      console.log(newProd.getInformation);  
+    }  
+});
+
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts){
+   console.log(prod.getInformation()); 
+}
 
 
