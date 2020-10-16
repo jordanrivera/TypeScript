f65e39470cf4613d20c75c3c7013c3ecf185f64b
Yordan Adonias Rivera Rios <yordanrivera@yahoo.com>
2020-10-16 12:39:12 +0200

    commit

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -11,13 +11,17 @@ function App() {
         ...prevTodos, 
         {id: Math.random().toString(), text: text}
       ]);
-
+   };
+   const todoDeleteHandler = (todoId: string) => {
+     setTodos(prevTodos => {
+       return prevTodos.filter(todo => todo.id !== todoId);
+     });
    };
 
   return (
     <div className="App">
       <NewTodo onAddTodo = {todoAddHandler}/>
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
     </div>
   );
 };
